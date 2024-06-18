1871c0e7d07d2ccc49b3f269b47996485a0ff368
Deepak Singh <deepsinghkumar01@gmail.com>
2024-06-18 12:14:03 +0530

    get & post url method created and db connection established.

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ const app = express();
 dotenv.config();
 
 
+const username = process.env.MONGODB_USERNAME;
+const password  = process.env.MONGODB_PASSWORD;
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.xzwoywt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,{
+    useNewUrlParser : true,
+    useUnifiedTopology:true,
+    
+});
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/pages/index.html")
+})
+
+app.post("/register",(req,res)=>{
+    try {
+        const{name,email,password} = req.body;
+    } catch (error) {
+        
+    }
+})
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
